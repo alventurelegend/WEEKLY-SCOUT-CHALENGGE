3371aa9c7c3122e1d3bb7cbb06e897c6382acd93
Ch Alga Praditya <newchalga04@gmail.com>
2025-06-12 21:13:13 +0700

    feat : "membuat fitur dashboard dan leaderboard"

---
+++ component/item/point.js
@@ -0,0 +1,50 @@
+const pointHistoryData = [
+  {
+    description: "Content creator",
+    points: 250,
+  },
+  {
+    description: "Pengabdian masyarakat",
+    points: 1500,
+  },
+  {
+    description: "Edukasi pramuka",
+    points: 100,
+  },
+  {
+    description: "Daily Checkin",
+    points: 150,
+  },
+  {
+    description: "Mengikuti lomba kebersihan",
+    points: 500,
+  },
+];
+
+const pointHistoryList = document.getElementById("pointHistoryList");
+
+function displayPointHistory(data) {
+  pointHistoryList.innerHTML = "";
+
+  if (data.length === 0) {
+    pointHistoryList.innerHTML =
+      '<div class="no-data-message">Tidak ada riwayat poin yang tersedia.</div>';
+    return;
+  }
+
+  data.forEach((item) => {
+    const historyItemDiv = document.createElement("div");
+    historyItemDiv.classList.add("history-item");
+
+    historyItemDiv.innerHTML = `
+                    <span>${item.description}</span>
+                    <span class="points">+${item.points}</span>
+                `;
+
+    pointHistoryList.appendChild(historyItemDiv);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  displayPointHistory(pointHistoryData);
+});
---
+++ public/js/leaderboard.js
@@ -0,0 +1,7 @@
+const Back = document.getElementsByClassName("back-button");
+
+if (Back) {
+  addEventListener("click", () => {
+    window.location.href = "../../component/page/dashboard.html";
+  });
+}
