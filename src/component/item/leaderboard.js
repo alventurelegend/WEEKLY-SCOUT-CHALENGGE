523383daec08d4935274de07fbe13a08c23aa658
Ch Alga Praditya <newchalga04@gmail.com>
2025-08-02 00:30:57 +0700

    fetch

--- src/component/item/leaderboard.js
+++ src/component/item/leaderboard.js
@@ -126,7 +126,7 @@ class LeaderboardDisplay extends HTMLElement {
 
     //CARA MEMASUKAN EVENT DARI DATABASE KE HTML
     try {
-      const response = await fetch("../../../api/leaderboard");
+      const response = await fetch("api/leaderboard");
       const data = await response.json();
 
       if (data.error) {
