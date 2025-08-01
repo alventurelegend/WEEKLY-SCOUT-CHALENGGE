ae3e7e2204ae17297c8088202fc01faab46d3640
Ch Alga Praditya <newchalga04@gmail.com>
2025-08-01 19:26:02 +0700

    feat tambahkan HEADER CORS

--- api/login.php
+++ api/login.php
@@ -1,7 +1,8 @@
         <?php 
             header("Access-Control-Allow-Origin: *");
+            header("Access-Control-Allow-Headers: *");
+            header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
             header("Content-Type: application/json");
-// .
 
             $logname = $_POST['loguser'];
             $logpass = $_POST['passlog'];
--- api/registrasi.php
+++ api/registrasi.php
@@ -1,5 +1,9 @@
     <?php 
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
         header("Content-Type: application/json");
+
 
         $nama = $_POST['user'];
         $pwd = $_POST['password'];
